150bf0c7dc5dd1a0e989aa0a1077fede306f8ad8
Xavier <xavieralves162@gmail.com>
2026-02-18 10:05:50 +0000

    Exc 6.21

--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,20 +1,34 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { addAnecdote } from '../services/anecdotes'
+
 const AnecdoteForm = () => {
-  const onCreate = (event) => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: addAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+    },
+  })
+
+  const handleSubmit = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
+    if (!content || content.length < 5) return
+    newAnecdoteMutation.mutate(content)
     event.target.anecdote.value = ''
-    console.log('new anecdote')
   }
 
   return (
-    <div>
-      <h3>create new</h3>
-      <form onSubmit={onCreate}>
+    <form onSubmit={handleSubmit}>
+      <div>
         <input name="anecdote" />
-        <button type="submit">create</button>
-      </form>
-    </div>
+      </div>
+      <button type="submit">create</button>
+    </form>
   )
 }
 
 export default AnecdoteForm
+
--- query-anecdotes/src/services/anecdotes.js
+++ query-anecdotes/src/services/anecdotes.js
@@ -2,8 +2,15 @@ const baseUrl = 'http://localhost:3001/anecdotes'
 
 export const getAnecdotes = async () => {
   const response = await fetch(baseUrl)
-  if (!response.ok) {
-    throw new Error('Anecdote service not available')
-  }
+  return response.json()
+}
+
+export const addAnecdote = async (content) => {
+  const anecdote = { content, votes: 0 }
+  const response = await fetch(baseUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(anecdote)
+  })
   return response.json()
 }
